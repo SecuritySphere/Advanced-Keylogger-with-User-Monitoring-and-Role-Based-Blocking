ffacbc75e307237b82755f8b85257bb2ef62eef8
SecuritySphere <kunikahpatil@gmail.com>
2024-09-25 13:10:55 +0530

    Add files via upload

---
+++ user_login_check.py
@@ -0,0 +1,11 @@
+import os
+
+blocked_users_file = r'C:/ProgramData/blocked_user.txt'
+username = os.getenv('USERNAME')
+
+if os.path.exists(blocked_users_file):
+    with open(blocked_users_file, 'r') as file:
+        blocked_users = file.read().splitlines()
+        if username in blocked_users:
+            print(f"Access denied for {username}")
+            os.system("shutdown -l")  # Exit with an error code to prevent login
